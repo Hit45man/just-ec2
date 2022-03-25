8da1d4647bae46d26685e6b4867eeb770674f6a5
Hitman1245 <rohit.kamble@blazeclan.com>
2022-03-25 15:28:01 +0530

    just ec2 sample code

---
+++ ec2.tf
@@ -0,0 +1,13 @@
+provider "aws" {
+  region = "ap-south-1"
+}
+
+
+
+
+resource "aws_instance" "my-ec2" {
+  ami               = "ami-03fa4afc89e4a8a09"
+  instance_type     = "t2.micro"
+  availability_zone = "ap-south-1a"
+ 
+}
