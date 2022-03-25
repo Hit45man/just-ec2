186b0a308c88b3b4bedfde2cdcfa0322d160e362
Hitman1245 <rohit.kamble@blazeclan.com>
2022-03-25 16:20:56 +0530

    added tags to ec2

--- ec2.tf
+++ ec2.tf
@@ -9,5 +9,9 @@ resource "aws_instance" "my-ec2" {
   ami               = "ami-03fa4afc89e4a8a09"
   instance_type     = "t2.micro"
   availability_zone = "ap-south-1a"
+
+  tags {
+    Name = "My Jenkins Terraform server" 
+  }
  
 }
